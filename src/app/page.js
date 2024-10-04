61f144c0431d6a6759ded385d7465ed3e2591dc7
Nihal-Sharma <72800662+Nihal-Sharma@users.noreply.github.com>
2024-10-04 19:39:57 +0530

    signup FE

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
+    <div className="flex bg-slate-400" >
+      <div>phot</div>
+      <div>t</div>
     </div>
   );
 }
